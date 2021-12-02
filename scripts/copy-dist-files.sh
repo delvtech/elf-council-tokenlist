9bb64976dcbf4fd079155da1d820607ca751a4f6
Matthew Brown <mattfdbrown@gmail.com>
2021-12-02 13:34:25 -0300

    copy scripts from elf-tokenlist

---
+++ scripts/copy-dist-files.sh
@@ -0,0 +1,10 @@
+# Copy the modules and types over to dist/
+cp compiled/src/index.js dist
+cp compiled/src/types.d.ts dist
+cp compiled/src/tags.js dist
+cp compiled/src/tags.d.ts dist
+
+
+# Copy over the *.address.json files to dist/
+cp -R src/addresses/*.addresses.json dist
+cp src/addresses/AddressesJsonFile.d.ts dist/AddressesJsonFile.d.ts
