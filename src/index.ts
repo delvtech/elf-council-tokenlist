1c438734ac25855b77e4be2b3a7693555207a767
Matthew Brown <mattfdbrown@gmail.com>
2021-12-10 11:40:28 -0300

    manually create index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,172 +1,98 @@
-
-/**
- * 🚨🚨🚨 THIS FILE IS AUTO-GENERATED. DO NOT EDIT MANUALLY. 🚨🚨🚨
- * 🚨🚨🚨 THIS FILE IS AUTO-GENERATED. DO NOT EDIT MANUALLY. 🚨🚨🚨
- * 🚨🚨🚨 THIS FILE IS AUTO-GENERATED. DO NOT EDIT MANUALLY. 🚨🚨🚨
- *
- * This is the index.ts file that will be compiled to JS and set as the 'main'
- * property in package.json. This makes it possible to use elf-tokenlist as a
- * normal package, ie: import { mainnetTokenList } from 'elf-tokenlist'
- *
- * See scripts/build-index.sh for details.
- *
- * 🚨🚨🚨 THIS FILE IS AUTO-GENERATED. DO NOT EDIT MANUALLY. 🚨🚨🚨
- * 🚨🚨🚨 THIS FILE IS AUTO-GENERATED. DO NOT EDIT MANUALLY. 🚨🚨🚨
- * 🚨🚨🚨 THIS FILE IS AUTO-GENERATED. DO NOT EDIT MANUALLY. 🚨🚨🚨
- */
-
-import { TokenList } from '@uniswap/token-lists';
-import { AddressesJsonFile } from 'src/addresses/AddressesJsonFile';
-
+import { TokenInfo, TokenList } from '@uniswap/token-lists';
+import { AddressesJsonFile } from 'addresses/AddressesJsonFile';
+import goerliAddressListJson from 'addresses/goerli.addresses.json';
 // TODO: add this when we have it
 // export const mainnetTokenList: TokenList = ;
-export const goerliTokenList: TokenList = {
-  "name": "Council goerli token list",
-  "logoURI": "https://element.fi/logo.svg",
-  "timestamp": "2021-12-06T18:45:02.121Z",
-  "version": {
-    "major": 0,
-    "minor": 1,
-    "patch": 0
-  },
-  "tokens": [
-    {
-      "chainId": 5,
-      "address": "0xa7E598Ab2c0df4A55feBFDD32ee6Cc64d2aE54eA",
-      "symbol": "ttk",
-      "decimals": 18,
-      "name": "testtoken"
-    },
-    {
-      "chainId": 5,
-      "address": "0x5264a36f4839F45d753701B5BDFc66c3f4ad2F96",
-      "name": "Element Core Voting Contract",
-      "decimals": 0,
-      "symbol": "",
-      "extensions": {
-        "dayInBlocks": 6496,
-        "baseQuorum": 10000,
-        "lockDuration": 19488,
-        "minProposalPower": 500,
-        "extraVoteTime": 32480
-      }
-    },
-    {
-      "chainId": 5,
-      "address": "0x43161786d23627C02f7D3AFD20489924b491b009",
-      "name": "Element GSC Core Voting Contract",
-      "decimals": 0,
-      "symbol": "",
-      "extensions": {
-        "dayInBlocks": 6496,
-        "baseQuorum": 5,
-        "lockDuration": 19488,
-        "minProposalPower": 1,
-        "extraVoteTime": 32480
-      }
-    },
-    {
-      "chainId": 5,
-      "address": "0xeA1f4ec61FEe5a7A6EC1A323e1d042282068a4d9",
-      "name": "Element Locking Vault",
-      "decimals": 0,
-      "symbol": "",
-      "extensions": {
-        "token": "0xa7E598Ab2c0df4A55feBFDD32ee6Cc64d2aE54eA",
-        "staleBlockLag": 100000000
-      }
-    },
-    {
-      "chainId": 5,
-      "address": "0x40E10a6C50b2Cf77a804aA9e32D69aEc919E3765",
-      "name": "Element Vesting Vault",
-      "decimals": 0,
-      "symbol": "",
-      "extensions": {
-        "token": "0xa7E598Ab2c0df4A55feBFDD32ee6Cc64d2aE54eA",
-        "staleBlockLag": 100000000
-      }
-    },
-    {
-      "chainId": 5,
-      "address": "0xf29658584ED4EB1FD22521D5A9437B20c9925dd6",
-      "name": "Element Governance Steering Committee Vault",
-      "decimals": 0,
-      "symbol": "",
-      "extensions": {
-        "coreVoting": "0x43161786d23627C02f7D3AFD20489924b491b009",
-        "votingPowerBound": 500,
-        "idleDuration": 345600
-      }
-    },
-    {
-      "chainId": 5,
-      "address": "0x847D90e827a11Da7c124992905CDf1F03F330fA8",
-      "name": "Element Optimistic Rewards Vault",
-      "decimals": 0,
-      "symbol": "",
-      "extensions": {
-        "pendingRoot": "0x0000000000000000000000000000000000000000000000000000000000000000",
-        "proposalTime": 0,
-        "proposer": "0xcF3b7bcBbcEFF836F81f6EAd914706E699267bca",
-        "challengePeriod": 604800,
-        "rewardsRoot": "0xb3bc27033f96d4ab101b807a06fef330523d308e202f9733d748412c008251aa",
-        "lockingVault": "0xA0bfb97aC29Fc4034db52F8783efdaA25A070E1B",
-        "token": "0xa7E598Ab2c0df4A55feBFDD32ee6Cc64d2aE54eA"
-      }
-    },
-    {
-      "chainId": 5,
-      "address": "0x90123455668bA2B12CA76962a88c2e1c20B17F62",
-      "name": "Element Airdrop Contract",
-      "decimals": 0,
-      "symbol": "",
-      "extensions": {
-        "rewardsRoot": "0x9aafc9b1d8798b80c68d556519e69c64fbe5a4b205bbdccec12375353a1bf784",
-        "lockingVault": "0xFd6e33F5321E5E138ad354F412f7eD9083757dae",
-        "expiration": "9999999999",
-        "token": "0x2b1a91De5B9C3Ad6439eeAeD0E481F8cf6E22601"
-      }
-    },
-    {
-      "chainId": 5,
-      "address": "0xa465170e7049d1D048ce179Cc0c864d13223a9E2",
-      "name": "Element Treasury",
-      "decimals": 0,
-      "symbol": "",
-      "extensions": {
-        "owner": "0xcF3b7bcBbcEFF836F81f6EAd914706E699267bca"
-      }
-    },
-    {
-      "chainId": 5,
-      "address": "0x73E0C9AdF3E0f5535dA3A0b4265Dd73962643426",
-      "name": "Element Timelock",
-      "decimals": 0,
-      "symbol": "",
-      "extensions": {
-        "waitTime": "1000"
-      }
-    }
-  ]
-};
-export const goerliAddresses: AddressesJsonFile = {
-  "addresses": {
-    "airdrop": "0x90123455668bA2B12CA76962a88c2e1c20B17F62",
-    "coreVoting": "0x5264a36f4839F45d753701B5BDFc66c3f4ad2F96",
-    "elementToken": "0xa7E598Ab2c0df4A55feBFDD32ee6Cc64d2aE54eA",
-    "gscCoreVoting": "0x43161786d23627C02f7D3AFD20489924b491b009",
-    "gscVault": "0xf29658584ED4EB1FD22521D5A9437B20c9925dd6",
-    "lockingVault": "0xeA1f4ec61FEe5a7A6EC1A323e1d042282068a4d9",
-    "optimisticGrants": "0x2e25d3bfACF854c9b80f4A5A66ca0C32950f6c6d",
-    "optimisticRewardsVault": "0x847D90e827a11Da7c124992905CDf1F03F330fA8",
-    "timeLock": "0x73E0C9AdF3E0f5535dA3A0b4265Dd73962643426",
-    "treasury": "0xa465170e7049d1D048ce179Cc0c864d13223a9E2",
-    "vestingVault": "0x40E10a6C50b2Cf77a804aA9e32D69aEc919E3765"
-  },
-  "chainId": 5
-};
-export { getTokenList } from 'src/getTokenList';
-export * from 'src/types';
+import goerliTokenListJson from 'tokenlists/goerli.tokenlist.json';
 
+export const goerliTokenList: TokenList = goerliTokenListJson as TokenList;
+export const goerliAddressList: AddressesJsonFile = goerliAddressListJson;
 
+export { getTokenList } from 'getTokenList';
+
+export type ElementGovernanceTokenInfo = TokenInfo;
+
+export interface AirdropContractInfo extends TokenInfo {
+  extensions: {
+    rewardsRoot: string;
+    lockingVault: string;
+    expiration: string;
+    token: string;
+  };
+}
+
+export interface CoreVotingContractInfo extends TokenInfo {
+  extensions: {
+    dayInBlocks: number;
+    baseQuorum: number;
+    lockDuration: number;
+    minProposalPower: number;
+    extraVoteTime: number;
+  };
+}
+
+export interface LockingVaultInfo extends TokenInfo {
+  extensions: {
+    token: string;
+    staleBlockLag: number;
+  };
+}
+
+export interface OptimisticRewardsVaultInfo extends TokenInfo {
+  extensions: {
+    pendingRoot: string;
+    proposalTime: number;
+    proposer: string;
+    challengePeriod: number;
+    rewardsRoot: string;
+    lockingVault: string;
+    token: string;
+  };
+}
+
+export interface OptimisticsGrantsContractInfo extends TokenInfo {
+  extensions: {
+    token: string;
+    solvency: string;
+  };
+}
+
+export interface VestingVaultInfo extends TokenInfo {
+  extensions: {
+    token: string;
+    staleBlockLag: number;
+  };
+}
+
+export interface GSCVaultInfo extends TokenInfo {
+  extensions: {
+    coreVoting: string;
+    votingPowerBound: number;
+    idleDuration: number;
+  };
+}
+
+export interface TimelockInfo extends TokenInfo {
+  extensions: {
+    waitTime: string;
+  };
+}
+
+export interface TreasuryInfo extends TokenInfo {
+  extensions: {
+    owner: string;
+  };
+}
+
+export type AnyTokenListInfo =
+  | TokenInfo
+  | ElementGovernanceTokenInfo
+  | AirdropContractInfo
+  | CoreVotingContractInfo
+  | LockingVaultInfo
+  | OptimisticRewardsVaultInfo
+  | OptimisticsGrantsContractInfo
+  | VestingVaultInfo
+  | GSCVaultInfo
+  | TimelockInfo
+  | TreasuryInfo;
