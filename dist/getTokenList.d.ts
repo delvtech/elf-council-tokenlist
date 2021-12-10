44551b0f263bedf42481b13b50f0435fcbda1923
Matthew Brown <mattfdbrown@gmail.com>
2021-12-10 11:38:50 -0300

    remove dist

--- dist/getTokenList.d.ts
+++
@@ -1,4 +0,0 @@
-import { TokenList } from "@uniswap/token-lists";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { AddressesJsonFile } from "src/addresses/AddressesJsonFile";
-export declare function getTokenList(hre: HardhatRuntimeEnvironment, addressesJson: AddressesJsonFile, name: string): Promise<TokenList>;
